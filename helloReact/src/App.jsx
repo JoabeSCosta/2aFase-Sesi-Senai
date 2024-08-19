bdf95575d104863159be2f69a3921bdd1129b32d
JoabeSCosta <joabesantoscosta777@gmail.com>
2024-08-19 17:07:44 -0300

    Adicionado helloReact

---
+++ helloReact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='titulo1'>Hello React JS fofinho</h1>
+      <p className='paragrafo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ab sit magni eius optio ea porro atque a quas amet. Adipisci, perspiciatis architecto? Quasi pariatur, tenetur odio modi nesciunt nulla?</p>
+      <p className='paragrafo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ab sit magni eius optio ea porro atque a quas amet. Adipisci, perspiciatis architecto? Quasi pariatur, tenetur odio modi nesciunt nulla?</p>
+      <p className='paragrafo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ab sit magni eius optio ea porro atque a quas amet. Adipisci, perspiciatis architecto? Quasi pariatur, tenetur odio modi nesciunt nulla?</p>
+    </>
+  )
+}
+
+export default App
