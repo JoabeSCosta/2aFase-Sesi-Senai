a088ae22e631a1692a992bad9067b5107943abc4
JoabeSCosta <joabesantoscosta777@gmail.com>
2024-08-26 17:27:37 -0300

    Farmarcia

---
+++ farmarcia/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Body from './components/Body'
+import Contato from './components/Contato'
+import Header from './components/Header'
+import Produtos from './components/Produtos'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Body />
+      <Produtos />
+      <Contato />
+    </>
+  )
+}
+
+export default App
---
+++ farmarcia/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header() {
+    return (
+      <div className='header-container'>
+        <img className='header-logo' src="/images/farmarcia-logo-pq.png" alt="logo-pq" />
+        <label className='header-titulo'>Farmárcia</label>
+      </div>
+    )
+  }
+  
+  export default Header
